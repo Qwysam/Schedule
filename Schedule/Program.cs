a16688fc5d5dd0faae2bcc18dedff7c6b83afc9c
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2021-02-10 22:39:40 +0200

    Class for data storage and parsing

--- Schedule/Program.cs
+++ Schedule/Program.cs
@@ -10,17 +10,27 @@ namespace Schedule
 {
     class Program
     {
+        public class ClassInfo
+        {
+            public string DayOfWeek;
+            public string StartTime;
+            public string EndTime;
+            public string Classroom;
+            public string Class;
+            public string Teacher;
+            public bool Blue = false;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            List<string> schedule = new List<string>();
+            List<ClassInfo> schedule = new List<ClassInfo>();
             StartCrawlAsync(schedule).GetAwaiter().GetResult();
-            foreach (string str in schedule)
-                Console.WriteLine(str);
+            Console.WriteLine("Complete");
         }
 
-        private static async Task StartCrawlAsync(List<string> data)
+        private static async Task StartCrawlAsync(List<ClassInfo> data)
         {
+            List<ClassInfo> DataList = new List<ClassInfo>();
             var url = "https://profkomstud.khai.edu/schedule/group/525b";
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(url);
@@ -29,22 +39,61 @@ namespace Schedule
             var table = htmlDocument.DocumentNode.Descendants("table")
                 .Where(node => node.GetAttributeValue("class", "").Equals("table")).ToList();
             var AlltdNodes = table[0].SelectNodes(".//td");
-            foreach(HtmlNode node in AlltdNodes)
+            //for checking if the next day started
+            string date_prev = "Понеділок\0";
+            string date_current = "";
+            string time_prev = "08:00 - 09:35";
+            string time_current = "";
+            data.Add(new ClassInfo());
+            foreach (HtmlNode node in AlltdNodes)
             {
-                if (node.InnerText[0] == '&')
+                if (node.InnerText != "")
                 {
-                    string[] tmp = node.InnerText.Split(';');
-                    char[] char_arr = new char[tmp.Length];
-                    for (int i = 0; i < tmp.Length-1; i++)
+                    if (node.InnerText[0] == '&')
                     {
-                        tmp[i] = tmp[i].Replace("&#","0");
-                        char_arr[i] = (char)Convert.ToInt32(tmp[i], 16);
+                        string[] tmp = node.InnerText.Split(';');
+                        char[] char_arr = new char[tmp.Length];
+                        for (int i = 0; i < tmp.Length - 1; i++)
+                        {
+                            tmp[i] = tmp[i].Replace("&#", "0");
+                            char_arr[i] = (char)Convert.ToInt32(tmp[i], 16);
 
+                        }
+                        date_current = new string(char_arr).Trim();
+                        if (date_current != date_prev)
+                        {
+                            date_prev = new string(char_arr).Trim();
+                            data.Add(new ClassInfo());
+                        }
+                        data[data.Count-1].DayOfWeek = date_prev;
                     }
-                    data.Add(new string(char_arr).Trim());
+                    if (node.InnerText.Contains(":"))
+                    {
+                        time_current = node.InnerText;
+                        if(time_current!= time_prev)
+                        {
+                            time_prev = time_current;
+                            data.Add(new ClassInfo());
+                        }
+                        string start_time = time_current.Split(" - ")[0];
+                        string end_time = time_current.Split(" - ")[1];
+                        data[data.Count - 1].StartTime = start_time;
+                        data[data.Count - 1].EndTime = end_time;
+                    }
+                    if (node.InnerText.Contains(","))
+                    {
+                        if (node.HasClass("x-blue2"))
+                            data[data.Count - 1].Blue = true;
+                        string[] tmp = node.InnerText.Split(',');
+                        data[data.Count - 1].Classroom = tmp[0].Trim();
+                        data[data.Count - 1].Class = tmp[1].Trim();
+                        data[data.Count - 1].Teacher = tmp[2].Trim();
+                    }
+                    if (node.InnerText == "")
+                    {
+                        data[data.Count - 1].Class = "No Class";
+                    }
                 }
-                else
-                    data.Add(node.InnerText.Trim());
             }
         }
     }
